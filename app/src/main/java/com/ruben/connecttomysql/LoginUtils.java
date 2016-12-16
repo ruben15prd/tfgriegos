43be66be2e22c081b3f68fe680e8de2bb7eaa5d3
ruben ramirez vera <ruben ramirez vera>
2016-12-16 14:52:17 +0100

    Creacion del proyecto

---
+++ app/src/main/java/com/ruben/connecttomysql/LoginUtils.java
@@ -0,0 +1,78 @@
+package com.ruben.connecttomysql;
+
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ruben on 15/12/2016.
+ */
+
+public  class LoginUtils {
+
+    public static List<String> compruebaErroresLogin(Activity activity) {
+        List<String> errores;
+        EditText nombre, contrasenya;
+        String nombreStr, contrasenyaStr;
+
+        errores = new ArrayList<String>();
+        nombre = (EditText) activity.findViewById(R.id.editText);
+        contrasenya = (EditText) activity.findViewById(R.id.editText2);
+
+        nombreStr = nombre.getText().toString();
+        contrasenyaStr = contrasenya.getText().toString();
+
+        if (nombreStr.matches("")) {
+            errores.add("Debes introducir un nombre");
+        }
+        if (contrasenyaStr.matches("")) {
+            errores.add("Debes introducir una contraseña");
+        }
+        return errores;
+    }
+
+    public static void visualizaErroresLogin(Activity activity) {
+        EditText nombre, contrasenya;
+        String nombreStr, contrasenyaStr;
+
+        nombre = (EditText) activity.findViewById(R.id.editText);
+        contrasenya = (EditText) activity.findViewById(R.id.editText2);
+
+        nombreStr = nombre.getText().toString();
+        contrasenyaStr = contrasenya.getText().toString();
+
+        if (nombreStr.matches("")) {
+            Toast.makeText(activity.getApplicationContext(), "Debes introducir un nombre", Toast.LENGTH_SHORT).show();
+        }
+        if (contrasenyaStr.matches("")) {
+            Toast.makeText(activity.getApplicationContext(), "Debes introducir una contraseña", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public static boolean compruebaUsuarioValido(String nombreEditText, String contrasenyaEditText, String nombreConsulta, String contrasenyaConsulta) {
+        boolean result;
+
+
+        if (nombreEditText.equals(nombreConsulta) && contrasenyaEditText.equals(contrasenyaConsulta)) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+
+        return result;
+
+    }
+
+    public static void visualizaErroresUsuarioValido(Activity activity,boolean bool) {
+
+        if (bool==false) {
+            Toast.makeText(activity.getApplicationContext(), "Las credenciales no son válidas", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ruben/connecttomysql/MainActivity.java
@@ -0,0 +1,152 @@
+package com.ruben.connecttomysql;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String url= "jdbc:mysql://138.68.102.13:3306/TFGRIEGOS";
+    private static final String user = "prueba";
+    private static final String pass = "irrigadino";
+
+    // Declaramos los elementos de la interfaz
+    private TextView nombre,contrasenya;
+    private EditText nombreLogin,contrasenyaLogin;
+
+    boolean usuarioValido;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inicializamos los elementos de la interfaz
+        nombre = (TextView) findViewById(R.id.textView);
+        contrasenya = (TextView) findViewById(R.id.textView6);
+
+        nombreLogin = (EditText) findViewById(R.id.editText);
+        contrasenyaLogin =  (EditText) findViewById(R.id.editText2);
+
+
+        Button buttonLogin = (Button) findViewById(R.id.button2);
+
+
+        //Definimos el comportamiento del boton login
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MyTask2().execute();
+            }
+        });
+
+    }
+
+
+    // Ejecutamos de forma asincrona, las acciones del boton login
+    private class MyTask2 extends AsyncTask<Void,Void,Void>{
+        private String nom="",nomEditText="",contra="",contraEditText="";
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            List<String> errores;
+
+
+            errores = new ArrayList<String>();
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url,user,pass);
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        // some code #3 (Write your code here to run in UI thread)
+
+                        //Obtenemos el texto de los campos usuario y contraseña que ha introducido el usuario
+                         nomEditText = nombreLogin.getText().toString();
+                         contraEditText= contrasenyaLogin.getText().toString();
+
+
+
+                        //Log.d("Debug", "El valor del usuario es: " + nomEditText);
+                        //Log.d("Debug", "El valor de la contraseña es: " + contraEditText);
+                    }
+                });
+
+                errores = LoginUtils.compruebaErroresLogin(MainActivity.this);
+                //Log.d("Debug", "Tamaño de la lista de errores: " + errores.size());
+                if(errores.size()==0){
+                    Statement st = con.createStatement();
+                    //Log.d("Debug", "Antes de la consulta el usuario: " + nomEditText);
+                    //Log.d("Debug", "Antes de la consulta la contraseña: " + contraEditText);
+                    String sql = "select * from PROPIETARY where name ='"+nomEditText+"' and password='"+contraEditText+"'";
+                    //Realizamos la consulta contra la base de datos
+                    final ResultSet rs = st.executeQuery(sql);
+
+                    //rs.next();
+
+                    while(rs.next()) {
+                        nom=rs.getString(3);
+                        contra=rs.getString(4);
+                        // Comprobamos si las credenciales que ha introducido son validas
+                        usuarioValido = LoginUtils.compruebaUsuarioValido(nomEditText,contraEditText,nom,contra);
+
+                        //Log.d("Debug", "He entrado");
+                    }
+
+
+                }
+
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result){
+            List<String> errores;
+            //nombre.setText(nom);
+            //contrasenya.setText(contra);
+
+            // Comprobamos y visualizamos los errores en caso de que fuera necesario
+            errores = LoginUtils.compruebaErroresLogin(MainActivity.this);
+            LoginUtils.visualizaErroresLogin(MainActivity.this);
+            LoginUtils.visualizaErroresUsuarioValido(MainActivity.this,usuarioValido);
+
+
+            if(errores.size()>0 || usuarioValido==false){
+                return;
+            }else{
+                // Si ha ido correctamente lo llevamos a la nueva ventana
+                Intent irActivity2 = new Intent (MainActivity.this, Main2Activity.class);
+                startActivity(irActivity2);
+            }
+
+
+
+
+            super.onPostExecute(result);
+        }
+    }
+
+
+
+
+
+}
